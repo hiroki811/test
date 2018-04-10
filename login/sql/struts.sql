f595605f621e18ec4fc1a78ded30ced05771181a
hiroki811 <hirokisoccer811@gmail.com>
2018-04-10 17:47:24 +0900

    2018/04/10

--- login/sql/struts.sql
+++ login/sql/struts.sql
@@ -1,16 +1,17 @@
 set names utf8;
 set foreign_key_checks = 0;
-dorop database if exists logindb;
+drop database if exists logindb;
 create database logindb;
 use logindb;
 
-ceate table user(
+
+create table user(
 id int,
 user_name varchar(255),
-passqord varchar(255));
-
+password varchar(255)
+);
 insert into user values
 (1,"taro","123"),
-(2."jiro","123"),
+(2,"jiro","123"),
 (3,"hanako","123"),
 (4,"saburo","123");
